c40a407ba622bdb61c03f8a14d73be0b3e501012
Devin Dwyer <devinjd@live.unc.edu>
2021-05-02 19:50:50 -0400

    Print the numbers with comma in between each arg

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 // Stores the arguments into a variable
 var args = process.argv.slice(2);
 
-console.log(args);
+// Loop for going through each argument
+for (var i = 0; i < args.length; i++) {
+    // Convert to string to be able to go through each digit
+    args[i].toString();
+    process.stdout.write(args[i]);
+
+    // Seperate arguments with a comma unless its the last argument
+    if (i != args.length - 1) {
+        process.stdout.write(",");
+    }
+}
